e9ffa47dc485d6d6362392bc3bb85eb81b6de127
Dawid Buchwald <dawid.buchwald@hotmail.com>
2019-05-09 21:10:16 +0200

    Added support for toggle button on Pin 4

--- simple_signal_3leds/simple_signal_3leds.ino
+++ simple_signal_3leds/simple_signal_3leds.ino
@@ -1,39 +1,79 @@
 #define PIN_0 0
 #define PIN_1 1
 #define PIN_2 2
+#define PIN_4 4
 
 #define RED_LED 4
 #define ORANGE_LED 2
 #define GREEN_LED 1
 
+#define NORMAL_MODE 0
+#define EMERGENCY_MODE 1
+
 #define CYCLE_SLEEP 200
 
-#define CYCLES_COUNT 4
+#define NORMAL_CYCLES_COUNT 4
+#define EMERGENCY_CYCLES_COUNT 2
 
-int cycles[CYCLES_COUNT]={4, 6, 1, 2};
-int delays[CYCLES_COUNT]={4, 1, 4, 1};
+int normalCycles[NORMAL_CYCLES_COUNT]={4, 6, 1, 2};
+int normalDelays[NORMAL_CYCLES_COUNT]={4, 1, 4, 1};
+int emergencyCycles[EMERGENCY_CYCLES_COUNT]={2, 0};
+int emergencyDelays[EMERGENCY_CYCLES_COUNT]={1, 1};
 int currentCycle;
 int currentPhase;
+int currentMode;
+int buttonPressed;
+int lastButtonState;
+int* cycles;
+int* delays;
+int cyclesCount;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(PIN_0, OUTPUT);
   pinMode(PIN_1, OUTPUT);
   pinMode(PIN_2, OUTPUT);
+  pinMode(PIN_4, INPUT);
+  currentMode=NORMAL_MODE;
+  cyclesCount=NORMAL_CYCLES_COUNT;
+  cycles=normalCycles;
+  delays=normalDelays;
   currentCycle=0;
   currentPhase=0;
+  lastButtonState=digitalRead(PIN_4);
 }
 
 void toggleLEDs(int state) {
-  
   digitalWrite(PIN_2, state & RED_LED ? HIGH : LOW);
   digitalWrite(PIN_1, state & ORANGE_LED ? HIGH : LOW);
   digitalWrite(PIN_0, state & GREEN_LED ? HIGH : LOW);
 }
+void toggleMode() {
+
+  if (currentMode == NORMAL_MODE) {
+    currentMode=EMERGENCY_MODE;
+    cyclesCount=EMERGENCY_CYCLES_COUNT;
+    cycles=emergencyCycles;
+    delays=emergencyDelays;
+  } else {
+    currentMode=NORMAL_MODE;
+    cyclesCount=NORMAL_CYCLES_COUNT;
+    cycles=normalCycles;
+    delays=normalDelays;
+  }
+
+  currentCycle=0;
+  currentPhase=0;
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (currentCycle>CYCLES_COUNT-1) {
+  buttonPressed = digitalRead(PIN_4);
+  if (!buttonPressed && lastButtonState) {
+    toggleMode();
+  }
+  lastButtonState = buttonPressed;
+  if (currentCycle>cyclesCount-1) {
     currentCycle=0;
   }
   toggleLEDs(cycles[currentCycle]);
